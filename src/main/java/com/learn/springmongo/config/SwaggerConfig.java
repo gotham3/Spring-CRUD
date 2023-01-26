0062de93903f39cc752b3df413689ab1e8fa45ec
Nandini <nandini@Nandinis-MacBook-Air.local>
2023-01-26 20:05:19 +0530

    remove apiInfo

--- src/main/java/com/learn/springmongo/config/SwaggerConfig.java
+++ src/main/java/com/learn/springmongo/config/SwaggerConfig.java
@@ -2,14 +2,11 @@ package com.learn.springmongo.config;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,22 +15,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 class SwaggerConfig implements WebMvcConfigurer {
 
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-        "Todo App REST API",
-        "Hi, this is the API documentation for my Todo Tasks App",
-        "API TOS",
-        "Terms of service",
-        new Contact("Nandini", "https://todo-spring.onrender.com/", "contact-us@gmail.com"),
-        "License of API",
-        "API license URL",
-        Collections.emptyList());
-  }
-
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(apiInfo())
+        //        .apiInfo(apiInfo())
         .select()
         .apis(RequestHandlerSelectors.basePackage("com.learn.springmongo.controller"))
         .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
