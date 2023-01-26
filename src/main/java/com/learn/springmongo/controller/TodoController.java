883b8ce2c00f09ab4ed532b08c65fd060dc32768
Nandini <nandini@Nandinis-MacBook-Air.local>
2023-01-26 19:43:39 +0530

    update documentation

--- src/main/java/com/learn/springmongo/controller/TodoController.java
+++ src/main/java/com/learn/springmongo/controller/TodoController.java
@@ -3,6 +3,10 @@ package com.learn.springmongo.controller;
 import com.learn.springmongo.model.TodoTask;
 import com.learn.springmongo.model.TodoTaskList;
 import com.learn.springmongo.service.TodoService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,38 +15,50 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/todo/")
 @Slf4j
+@Tag(name = "Todo App APIs")
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Todo tasks service APIs",
+            description = "Documentation of Todo tasks application APIs"))
 public class TodoController {
   @Autowired private TodoService todoService;
 
+  @Operation(summary = "Get task details by ID")
   @GetMapping("/{id}")
   public ResponseEntity<Object> getTaskById(@PathVariable String id) {
     TodoTask todoTask = todoService.getTaskById(id);
     return ResponseEntity.ok().body(todoTask);
   }
 
+  @Operation(summary = "Get all task details")
   @GetMapping("/")
-  public ResponseEntity<Object> getAllTasksById() {
+  public ResponseEntity<Object> getAllTasks() {
     TodoTaskList todoTaskList = todoService.getTodoList();
     return ResponseEntity.ok().body(todoTaskList);
   }
 
+  @Operation(summary = "Get total number of tasks")
   @GetMapping("/counttasks")
   public ResponseEntity<Object> getCountOfAllTasks() {
     return ResponseEntity.ok(todoService.getNumberOfTasks());
   }
 
+  @Operation(summary = "Create new task")
   @PostMapping("/addtask")
   public ResponseEntity<Object> addTaskToList(@RequestBody TodoTask todoTask) {
     String taskId = todoService.addTaskToList(todoTask);
     return ResponseEntity.ok(taskId);
   }
 
+  @Operation(summary = "Edit task details")
   @PutMapping("/edittask")
   public ResponseEntity<Object> editTask(@RequestBody TodoTask todoTask) {
     boolean success = todoService.updateTodoList(todoTask);
     return ResponseEntity.ok(success);
   }
 
+  @Operation(summary = "Delete task by ID")
   @DeleteMapping("/{id}")
   public ResponseEntity<Object> deleteTaskById(@PathVariable String id) {
     todoService.deleteItem(id);
